126c467edd8065d2d2b60b3761215df98ed07a28
DesertWhale76 <hadidknazev@gmail.com>
2026-05-19 12:37:53 +0600

    Testing installer...

---
+++ apps/supertux_installer.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.local/share/applications
+mkdir -p ~/.apps/supertux/icon
+wget -P ~/.apps/supertux/icon capy-os-store.github.io/icons/supertux.png
+wget -P ~/.apps/supertux github.com/SuperTux/supertux/releases/download/v0.7.0/SuperTux-v0.7.0.glibc2.38-x86_64.AppImage
+wget -P ~/.local/share/applications capy-os-store.github.io/desktop/supertux.desktop
